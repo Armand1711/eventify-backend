fcaa6a88bee5df02eba4c9dd8f66616010fc3900
Armand1711 <125480832+Armand1711@users.noreply.github.com>
2025-06-08 12:31:33 +0200

    reverted to old repo

--- EventifyBackend/obj/Debug/net8.0/EventifyBackend.AssemblyInfo.cs
+++ EventifyBackend/obj/Debug/net8.0/EventifyBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventifyBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e6e768666297f6490395a617a7427aa4c0e3330")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c51694cec9f85fece91debc1ae62eca58c0b5849")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventifyBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventifyBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
