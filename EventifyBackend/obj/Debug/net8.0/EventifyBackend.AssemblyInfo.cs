e0db2f7d1fc978b356f22c1824c6b03ecb90b381
Armand1711 <125480832+Armand1711@users.noreply.github.com>
2025-11-16 09:35:43 +0200

    updated the appsettings.json to make sure the backend will connect to the database

--- EventifyBackend/obj/Debug/net8.0/EventifyBackend.AssemblyInfo.cs
+++ EventifyBackend/obj/Debug/net8.0/EventifyBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventifyBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c2a5bf55d0dd9be9cdfd5415484d6914296604d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffe69950267a63507da6b4f6cb917fa7fddab6b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventifyBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventifyBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
